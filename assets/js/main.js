88a371c92e72214379192f957d3fbee3b2a0efa3
Ella Choy <egal21de@gmail.com>
2021-08-30 02:42:18 +0200

    game

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,5 +1,28 @@
 console.log("proj");
 
-function do_rock(){
-    console.log(document.getElementById("do_rock").value);
-}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// let computer= random() %3;
+// let user= 
+// 0=rock
+// 1=Scissors
+// 2=Paper
+
+
+// if(computer == 0 && user ==0);{
+//     document.getElementById('result').innerHTML='unentschieden'
+// }else if (computer == 0 && user == ){
+//     document.getElementById('result').innerHTML='computer win'
+// }
