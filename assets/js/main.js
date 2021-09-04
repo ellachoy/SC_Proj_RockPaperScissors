c84e4ce4c28a21d80fa57068e321e1dd1a53b4b7
Ella Choy <egal21de@gmail.com>
2021-09-04 20:53:39 +0200

    final

--- assets/js/main.js
+++ assets/js/main.js
@@ -2,7 +2,42 @@ console.log("proj");
 
 
 
+function get_results(){
+    // console.log(document.getElementById('btn').checked).value;
+    // c_box= Number(document.querySelector('input[type=button]').checked.value);
 
+    com_cal= Math.floor(Math.random()*3);
+    console.log(`com : ${com_cal}`);
+    str_Input= ['Rock', 'Paper', 'Scissors'];
+    str_Out= (` user:  ${str_Input[user]}  VS  computer : ${str_Input[com_cal]} =`);
+
+    if(user== com_cal){
+        str_Out = str_Out + "draw!"
+    }else if ((user == 0 && com_cal == 1) || (user == 1 && com_cal == 2)|| (user == 2 && com_cal == 0)){
+        str_Out = str_Out + "Computer win!"
+    }else{
+        str_Out = str_Out + "User win"
+    }
+     document.getElementById('result').innerHTML =str_Out;   
+        
+}
+
+// user
+let user=0;
+
+function do_rock(){
+    user=0;
+    
+}
+
+function do_scissor(){
+    user=1;
+    
+}
+function do_paper(){
+    user=2;
+    
+}
 
 
 
